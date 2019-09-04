4e0ade7c1598caff74529fa333fd2a556b22b160
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2019-09-03 22:41:13 -0400

    backend funcional

--- db/controllers/fighterController.js
+++ db/controllers/fighterController.js
@@ -1,5 +1,79 @@
 const express = require('express');
 
-cosnt router = express.Router();
+const router = express.Router();
 
 const Fighter = require('../models/Fighter');
+
+router.get('/', async (req, res) => {
+  try {
+    const allFighters = await Fighter.find();
+    res.json({
+      code: 200,
+      message: "success",
+      data: allFighters
+    })
+  } catch(err){
+    res.send(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try{
+    const createdFighter = await Fighter.create(req.body);
+    res.json({
+      status: {
+        code: 201,
+        message: 'success'
+      },
+      data: createdFighter
+    })
+  } catch(err){
+    res.send(err)
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try{
+    const foundFighter = await Fighter.findById(req.params.id);
+    res.json({
+      status: {
+        code: 200,
+        message: 'success'
+      },
+      data: foundFighter
+    })
+  }catch(err){
+    res.send(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedFighter = Fighter.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json({
+      status: {
+        code: 200,
+        message: 'updated successfully'
+      },
+      data: updatedFighter
+    })
+  } catch(err){
+    res.send(err)
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedFighter = await Fighter.findByIdAndRemove(req.params.id);
+    res.json({
+      status: {
+        code: 200,
+        message: 'deleted successfully'
+      }
+    })
+  } catch(err){
+    res.send(err);
+  }
+});
+
+module.exports = router;
--- db/server.js
+++ db/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('./db/server.js');
+// require('./db/server.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -19,4 +20,14 @@ app.use('/fighter', fighterController);
 
 app.listen(process.env.PORT || 9000, () => {
   console.log('listening on PORT 9000');
+})
+
+mongoose.connect('mongodb://localhost/fighters', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('mongoose is connected');
 })
